ce0fa1992cc29b1c7c88f9e9c2a6caf104c89fd8
Vanderlei Chepanski <vchepanski@outlook.com>
2025-06-04 22:13:03 -0300

    first deploy: conectando com PostgreSQL na Laravel Cloud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return '<h1>Chama na vozzzzx! 🚀</h1>';
+});
+Route::get('/test-db', function () {
+    return DB::select('SELECT NOW()');
 });
