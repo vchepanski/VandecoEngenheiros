dcbc62a8b1b3470e57988857268f22155f53235b
Vanderlei Chepanski <vchepanski@outlook.com>
2025-06-08 23:36:24 -0300

    First commit: personal task manager

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::match(['get', 'post'], '/access', function (Request $request) {
     if ($request->isMethod('post')) {
         if ($request->input('password') === 'Dev-Vanderlei@123') {
             session(['access_granted' => true]);
-            return redirect()->intended('/tasks');
+            return redirect()->intended('/');
         }
 
         return back()->withErrors(['password' => 'Senha incorreta.']);
@@ -23,7 +23,7 @@ Route::match(['get', 'post'], '/access', function (Request $request) {
 })->name('access');
 Route::middleware('protected')->group(function () {
 Route::resource('usuarios', UserController::class);
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
