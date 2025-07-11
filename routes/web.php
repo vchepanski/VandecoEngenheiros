6880fb0b7c15722ada729a256ca1faef602ab1d2
Vanderlei Chepanski <vchepanski@outlook.com>
2025-07-10 21:55:00 -0300

    Password .ev

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::match(['get', 'post'], '/access', function (Request $request) {
     if ($request->isMethod('post')) {
-        if ($request->input('password') === 'Dev-Vanderlei@123') {
+        if ($request->input('password') === env('ACCESS_PASSWORD')) {
             session(['access_granted' => true]);
             return redirect()->intended('/');
         }
 
-        return back()->withErrors(['password' => 'Senha incorreta.']);
+        return back()->withErrors(['password' => 'Pamonha.']);
     }
 
     return response()->view('auth.access');
